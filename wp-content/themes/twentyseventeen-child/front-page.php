ebbfc64fd20748656365add18facdd0fe02318a1
AndreyYeryomin <andrey.yeryomin11@gmail.com>
2018-04-27 14:03:40 +0300

    add menu and created category

--- wp-content/themes/twentyseventeen-child/front-page.php
+++ wp-content/themes/twentyseventeen-child/front-page.php
@@ -1,10 +1,66 @@
-<?php wp_head();?>
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js no-svg">
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+
+    <?php wp_head(); ?>
+</head>
 <body>
+<header>
+    <div class="container">
+        <div class="logo">
+            <a href="<?php echo get_home_url(); ?>"><img src="<?php echo get_stylesheet_directory_uri();?>/assets/img/logo.png" alt=""></a>
+        </div>
+        <nav>
+            <?php wp_nav_menu(); ?>    
+        </nav>
+    </div>
+</header>
+
 <div class="main">
 <?php
 echo do_shortcode('[slider id="23"]');
+if( is_front_page()){
+$args = array(
+    'taxonomy'   => "product_cat",
+    'number'     => $number,
+    'orderby'    => $orderby,
+    'order'      => $order,
+    'hide_empty' => $hide_empty,
+    'include'    => $ids
+);
+$product_categories = get_terms($args);
+print_r($product_categories);
+}
 ?>
 </div>
-<?php wp_footer();?>
+    <footer id="colophon" class="site-footer" role="contentinfo">
+        <div class="wrap">
+            <?php wp_footer(); ?>
+            <?php
+            get_template_part( 'template-parts/footer/footer', 'widgets' );
+
+            get_template_part( 'template-parts/footer/site', 'info' );
+            ?>
+        </div><!-- .wrap -->
+    </footer><!-- #colophon -->
+</body>
+</html>
 
 
