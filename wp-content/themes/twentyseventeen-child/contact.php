31be3b5da637e735eb26329abb2fc686e747d083
AndreyYeryomin <andrey.yeryomin11@gmail.com>
2018-04-27 19:06:25 +0300

    some fix

--- wp-content/themes/twentyseventeen-child/contact.php
+++ wp-content/themes/twentyseventeen-child/contact.php
@@ -8,7 +8,7 @@
 ?>
 <div class="contact-block">
     <h2>CONTACT US</h2>
-    <p></p>
+    <p>Please contact us for all inquiries and purchase options.</p>
     <div class="contact-form">
         <?php echo do_shortcode('[contact-form-7 id="58" title="Contact form 1"]'); ?>
     </div>
